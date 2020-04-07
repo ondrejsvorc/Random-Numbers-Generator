19ce710df50007fd070b9042eb0c5896b2e891b1
ondrejsvorc <52735242+ondrejsvorc@users.noreply.github.com>
2020-04-07 18:07:38 +0200

    Update Form1.cs

--- Random_Numbers_Generator/Form1.cs
+++ Random_Numbers_Generator/Form1.cs
@@ -7,7 +7,8 @@ namespace Random_Numbers_Generator
     {
         Random random = new Random();
 
-        #region Initialization of the form
+
+        // Initialization of the form
         public Home()
         {
             InitializeComponent();
@@ -19,22 +20,22 @@ namespace Random_Numbers_Generator
             txtFrom.MaxLength = 9;
             txtTo.MaxLength = 9;
             txtAmount.MaxLength = 9;
-
         }
-        #endregion
 
 
-        #region FUNCTIONS
+        
 
-        #region FUNCTION: Shows the "Re-Generate" button
+        // FUNCTION: Shows the "Re-Generate" button
         void AllowRegenerate()
         {
             btnReGenerate.Show();
             btnGenerate.Text = "Generate more";
         }
-        #endregion
 
-        #region FUNCTION: Re-generates the numbers to the main text box
+
+
+
+        // FUNCTION: Re-generates the numbers to the main text box
         void RandomNumsToMainTxtBox()
         {
             try
@@ -53,17 +54,14 @@ namespace Random_Numbers_Generator
 
                 }
                 txtRandomNumbers.Text += "\n" + "\n";
-
-
             }
             catch { }
-
-
-
         }
-        #endregion
 
-        #region FUNCTION: Generates more random numbers
+
+
+
+        // FUNCTION: Generates more random numbers
         void GenerateMoreToMainTxtBox()
         {
             try
@@ -83,7 +81,7 @@ namespace Random_Numbers_Generator
                 {
                     nums = random.Next(valueFrom, valueTo);
 
-                    txtRandomNumbers.Text += nums.ToString() + "\n";  // txtRandomNumbers.Text = txtRandomNumbers.Text + ...
+                    txtRandomNumbers.Text += nums.ToString() + "\n";  
                     newTxt = txtRandomNumbers.Text;
 
                 }
@@ -93,14 +91,11 @@ namespace Random_Numbers_Generator
 
 
         }
-        #endregion
-
-        #endregion
 
 
-        #region EVENTS: TextChanged
 
-        #region EVENT: TxtFrom_TextChanged
+
+        // EVENT: TxtFrom_TextChanged
         private void TxtFrom_TextChanged(object sender, EventArgs e)
         {
             int numFrom;
@@ -123,9 +118,9 @@ namespace Random_Numbers_Generator
                 btnReGenerate.Enabled = false;
             }
         }
-        #endregion
 
-        #region EVENT: TxtAmount_TextChanged
+
+        // EVENT: TxtAmount_TextChanged
         private void TxtAmount_TextChanged(object sender, EventArgs e)
         {
             int numAmount;
@@ -151,37 +146,31 @@ namespace Random_Numbers_Generator
                 btnReGenerate.Enabled = false;
             }
         }
-        #endregion
 
-        #region TxtRandomNumbers_TextChanged
+
+        // TxtRandomNumbers_TextChanged
         private void TxtRandomNumbers_TextChanged(object sender, EventArgs e)
         {
             txtRandomNumbers.SelectionStart = txtRandomNumbers.Text.Length;
             txtRandomNumbers.ScrollToCaret();
         }
-        #endregion
-
-        #endregion
 
 
-        #region EVENTS: KeyPress
 
-        #region EVENT: TxtFrom_KeyPress
+
+
+        // EVENT: TxtFrom_KeyPress
         private void TxtFrom_KeyPress(object sender, KeyPressEventArgs e)
         {
-            //if (!(e.KeyChar >= 48 && e.KeyChar <= 57))
-            //{
-            //    e.Handled = true;
-            //}
-
             if (!char.IsNumber(e.KeyChar) && (Keys)e.KeyChar != Keys.Back)
             {
                 e.Handled = true;
             }
         }
-        #endregion
 
-        #region EVENT: TxtTo_KeyPress
+
+
+        // EVENT: TxtTo_KeyPress
         private void TxtTo_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsNumber(e.KeyChar) && (Keys)e.KeyChar != Keys.Back)
@@ -189,9 +178,11 @@ namespace Random_Numbers_Generator
                 e.Handled = true;
             }
         }
-        #endregion
+        
+        
+        
 
-        #region EVENT: TxtAmount_KeyPress
+        // EVENT: TxtAmount_KeyPress
         private void TxtAmount_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsNumber(e.KeyChar) && (Keys)e.KeyChar != Keys.Back)
@@ -199,65 +190,25 @@ namespace Random_Numbers_Generator
                 e.Handled = true;
             }
         }
-        #endregion
-
-        #endregion
 
 
-        #region BUTTONS: Generate and Re-Generate numbers
 
-        #region BUTTON: Generate 
+
+
+        // BUTTON: Generate 
         private void BtnGenerate_Click(object sender, EventArgs e)
         {
             GenerateMoreToMainTxtBox();
             AllowRegenerate();
         }
-        #endregion
 
-        #region BUTTON: Re-Generate
+
+        // BUTTON: Re-Generate
         private void BtnReGenerate_Click(object sender, EventArgs e)
         {
             RandomNumsToMainTxtBox();
         }
-
-
-
-        #endregion
-
-        #endregion
-
-
-
-
-
-        /*MAYBE
-         * PUT CZECH, ENGLISH AND GERMAN FLAG IN THE RIGHT TOP CORNER - IF ONE OF THE FLAGS IS CLICKED ON, LANGUAGE CHANGES
-         * 
-         *
-         */
-
-
-        /* TO-DO
-         * 
-         * 
-        */
-
-        /* DONE
-        * 
-        * MAKE THE RE-GENERATE FUNCTION WORK
-        * LIMIT THE txtAmount AND THE txtTo TO 999999999 OTHERWISE THE PROGRAMME CRASHES
-        * txtFrom MUST BE LOWER THAN txtTo
-        * 
-        * 
-        * 
-        * 
-        */
-
-
-
-
-
-
+        
 
     }
 }
